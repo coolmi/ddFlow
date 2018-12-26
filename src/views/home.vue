ad57cb62812f4f8c33db92d0fdcb525b92ecabdd
dongdong.li <dongdong.li@gmkholdings.com>
2018-12-26 09:38:05 +0800

    提交

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <group title="用户基础信息" labelWidth="6.5rem" gutter="0" labelMarginRight="1rem">
-      <cell title="扣除年度" v-model="userInfo.annualDeduction" value-align="right"></cell>
-      <cell title="纳税人姓名" v-model="userInfo.ename" value-align="right"></cell>
-      <cell title="纳税人证件类型" v-model="userInfo.certtype" value-align="right"></cell>
-      <cell title="纳税人证件号码" v-model="userInfo.certno" value-align="right"></cell>
-      <cell title="联系方式" v-model="userInfo.telno" value-align="right"></cell>
-      <cell title="纳税人识别号" v-model="userInfo.identifyNum" value-align="right"></cell>
-      <x-input title="联系地址" v-model="userInfo.address" value-align="right"></x-input>
-      <x-input title="电子邮箱" v-model="userInfo.email" value-align="right"></x-input>
-      <datetime title="修改日期" v-model="userInfo.dateModified" value-align="right"></datetime>
-      <x-switch title="配偶情况" :value-map="['0', '1']" v-model="userInfo.spouseStatus" @click.native="postatus"></x-switch>
-      <cell title="配偶姓名" v-model="userInfo.spouseName" v-show="poflag" value-align="right"></cell>
-      <cell title="配偶证件类别" v-model="userInfo.spouseIDType" v-show="poflag" value-align="right"></cell>
-      <cell title="配偶身份号码" v-model="userInfo.spouseIDNum" v-show="poflag" value-align="right"></cell>
+      <!--<cell title="扣除年度" v-model="userInfo.annualDeduction" value-align="right"></cell>-->
+      <!--<cell title="纳税人姓名" v-model="userInfo.ename" value-align="right"></cell>-->
+      <!--<cell title="纳税人证件类型" v-model="userInfo.certtype" value-align="right"></cell>-->
+      <!--<cell title="纳税人证件号码" v-model="userInfo.certno" value-align="right"></cell>-->
+      <!--<cell title="联系方式" v-model="userInfo.telno" value-align="right"></cell>-->
+      <!--<cell title="纳税人识别号" v-model="userInfo.identifyNum" value-align="right"></cell>-->
+      <!--<x-input title="联系地址" v-model="userInfo.address" value-align="right"></x-input>-->
+      <!--<x-input title="电子邮箱" v-model="userInfo.email" value-align="right"></x-input>-->
+      <!--<datetime title="修改日期" v-model="userInfo.dateModified" value-align="right"></datetime>-->
+      <!--<x-switch title="配偶情况" :value-map="['0', '1']" v-model="userInfo.spouseStatus" @click.native="postatus"></x-switch>-->
+      <!--<cell title="配偶姓名" v-model="userInfo.spouseName" v-show="poflag" value-align="right"></cell>-->
+      <!--<cell title="配偶证件类别" v-model="userInfo.spouseIDType" v-show="poflag" value-align="right"></cell>-->
+      <!--<cell title="配偶身份号码" v-model="userInfo.spouseIDNum" v-show="poflag" value-align="right"></cell>-->
     </group>
   </div>
 </template>
